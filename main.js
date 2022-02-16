77078e98bc7766befe44427cc5c145c5a8fd7c3d
Federico Brid <federicobrid@gmai.com>
2022-02-15 22:15:06 -0300

    memory card sin finalizar 2

--- main.js
+++ main.js
@@ -1,50 +1,49 @@
 document.addEventListener('DOMContentLoaded', () =>{
 
     const card = [
-        //20 cards
-        {img:"img/beer.png", name:'beer'},
-        {img:"img/bread.png", name:'bread'},
-        {img:"img/burger.png", name:'burger'},
-        {img:"img/chicken.png", name:'chicken'},
-        {img:"img/coockie.png", name:'coockie'},
-        {img:"img/cupcake.png", name:'cupcake'},
-        {img:"img/donut.png", name:'donut'},
-        {img:"img/flan.png", name:'flan'},
-        {img:"img/fried-egg.png", name:'fried-egg'},
-        {img:"img/hotdog.png", name:'hotdog'},
-        {img:"img/ice-cream.png", name:'ice-cream'},
-        {img:"img/croissant.png", name:'croissant'},
-        {img:"img/milk.png", name:'milk'},
-        {img:"img/pizza.png", name:'pizza'},
-        {img:"img/popcorn.png", name:'popcorn'},
-        {img:"img/ramen.png", name:'ramen'},
-        {img:"img/sandwich.png", name:'sandwich'},
-        {img:"img/sushi.png", name:'sushi'},
-        {img:"img/tacos.png", name:'tacos'},
-        {img:"img/pancakes.png", name:'pancakes'},
-        //repeat cards
-        {img:"img/beer.png", name:'beer'},
-        {img:"img/bread.png", name:'bread'},
-        {img:"img/burger.png", name:'burger'},
-        {img:"img/chicken.png", name:'chicken'},
-        {img:"img/coockie.png", name:'coockie'},
-        {img:"img/cupcake.png", name:'cupcake'},
-        {img:"img/donut.png", name:'donut'},
-        {img:"img/flan.png", name:'flan'},
-        {img:"img/fried-egg.png", name:'fried-egg'},
-        {img:"img/hotdog.png", name:'hotdog'},
-        {img:"img/ice-cream.png", name:'ice-cream'},
-        {img:"img/croissant.png", name:'croissant'},
-        {img:"img/milk.png", name:'milk'},
-        {img:"img/pizza.png", name:'pizza'},
-        {img:"img/popcorn.png", name:'popcorn'},
-        {img:"img/ramen.png", name:'ramen'},
-        {img:"img/sandwich.png", name:'sandwich'},
-        {img:"img/sushi.png", name:'sushi'},
-        {img:"img/tacos.png", name:'tacos'},
-        {img:"img/pancakes.png", name:'pancakes'},
+        //20 ci
+        {srcImg:"img/beer.png", name:'beer'},
+        {srcImg:"img/bread.png", name:'bread'},
+        {srcImg:"img/burger.png", name:'burger'},
+        {srcImg:"img/chicken.png", name:'chicken'},
+        {srcImg:"img/coockie.png", name:'coockie'},
+        {srcImg:"img/cupcake.png", name:'cupcake'},
+        {srcImg:"img/donut.png", name:'donut'},
+        {srcImg:"img/flan.png", name:'flan'},
+        {srcImg:"img/fried-egg.png", name:'fried-egg'},
+        {srcImg:"img/hotdog.png", name:'hotdog'},
+        {srcImg:"img/ice-cream.png", name:'ice-cream'},
+        {srcImg:"img/croissant.png", name:'croissant'},
+        {srcImg:"img/milk.png", name:'milk'},
+        {srcImg:"img/pizza.png", name:'pizza'},
+        {srcImg:"img/popcorn.png", name:'popcorn'},
+        {srcImg:"img/ramen.png", name:'ramen'},
+        {srcImg:"img/sandwich.png", name:'sandwich'},
+        {srcImg:"img/sushi.png", name:'sushi'},
+        {srcImg:"img/tacos.png", name:'tacos'},
+        {srcImg:"img/pancakes.png", name:'pancakes'},
+        //repeat is
+        {srcImg:"img/beer.png", name:'beer'},
+        {srcImg:"img/bread.png", name:'bread'},
+        {srcImg:"img/burger.png", name:'burger'},
+        {srcImg:"img/chicken.png", name:'chicken'},
+        {srcImg:"img/coockie.png", name:'coockie'},
+        {srcImg:"img/cupcake.png", name:'cupcake'},
+        {srcImg:"img/donut.png", name:'donut'},
+        {srcImg:"img/flan.png", name:'flan'},
+        {srcImg:"img/fried-egg.png", name:'fried-egg'},
+        {srcImg:"img/hotdog.png", name:'hotdog'},
+        {srcImg:"img/ice-cream.png", name:'ice-cream'},
+        {srcImg:"img/croissant.png", name:'croissant'},
+        {srcImg:"img/milk.png", name:'milk'},
+        {srcImg:"img/pizza.png", name:'pizza'},
+        {srcImg:"img/popcorn.png", name:'popcorn'},
+        {srcImg:"img/ramen.png", name:'ramen'},
+        {srcImg:"img/sandwich.png", name:'sandwich'},
+        {srcImg:"img/sushi.png", name:'sushi'},
+        {srcImg:"img/tacos.png", name:'tacos'},
+        {srcImg:"img/pancakes.png", name:'pancakes'},
     ]
-    
     //array card random...
     card.sort(()=>Math.random() - 0.5);
 
@@ -56,12 +55,12 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     function printCards() {
         for (let i = 0; i < card.length; i++) {
-            const imgCard = document.createElement("img");
-            imgCard.setAttribute('src', 'img/chef.png');
-            imgCard.setAttribute('idCard', i);
-            imgCard.addEventListener('click', flipCard)
+            const srcImgCard = document.createElement("img");
+            srcImgCard.setAttribute('src', 'img/chef.png');
+            srcImgCard.setAttribute('idCard', i);
+            srcImgCard.addEventListener('click', flipCard)
     
-            containerCard.appendChild(imgCard);
+            containerCard.appendChild(srcImgCard);
         }
     }
 
@@ -70,12 +69,7 @@ document.addEventListener('DOMContentLoaded', () =>{
         const cardOne = arrayCardId[0];
         const cardTwo = arrayCardId[1];
 
-        if (cardOne == cardTwo) {
-            console.log('matchhh');
-            cards[cardOne].setAttribute('src', 'img/beer.png');
-            cards[cardTwo].setAttribute('src', 'img/beer.png');
-        }
-        else if (arrayChosen[0] === arrayChosen[1]){
+        if (arrayChosen[0] === arrayChosen[1]){
             cards[cardOne].removeEventListener('click', flipCard);
             cards[cardTwo].removeEventListener('click', flipCard);
             arrayCardWon.push(arrayChosen);
@@ -95,7 +89,7 @@ document.addEventListener('DOMContentLoaded', () =>{
         let cardId = this.getAttribute('idCard');
         arrayChosen.push(card[cardId].name);
         arrayCardId.push(cardId);
-        this.setAttribute('src', card[cardId].img);
+        this.setAttribute('src', card[cardId].srcImg);
         if (arrayChosen.length === 2) {
             setTimeout(check, 500);
         }
